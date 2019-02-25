08daa7589ea998201b9f23d349d417ad60cbb0a3
gdm-1718-jantemme <jantemme@student.arteveldehs.be>
2019-02-25 19:26:43 +0100

    changes

--- rect_animated.py
+++ rect_animated.py
@@ -9,12 +9,128 @@ sense.clear()
 def randNumb(endOfRange):
     return random.randint(0,endOfRange)
 
+## algoritme niet gevonden, hardcoden dan maar
+
+a = (0,0,0)
+b = (100,100,100)
+
+rect1 = [
+    b, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect2 = [
+    b, b, a, a, a, a, a, a,
+    b, b, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect3 = [
+    b, b, b, a, a, a, a, a,
+    b, a, b, a, a, a, a, a,
+    b, b, b, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect4 = [
+    b, b, b, b, a, a, a, a,
+    b, a, a, b, a, a, a, a,
+    b, a, a, b, a, a, a, a,
+    b, b, b, b, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect5 = [
+    b, b, b, b, b, a, a, a,
+    b, a, a, a, b, a, a, a,
+    b, a, a, a, b, a, a, a,
+    b, a, a, a, b, a, a, a,
+    b, b, b, b, b, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect6 = [
+    b, b, b, b, b, b, a, a,
+    b, a, a, a, a, b, a, a,
+    b, a, a, a, a, b, a, a,
+    b, a, a, a, a, b, a, a,
+    b, a, a, a, a, b, a, a,
+    b, b, b, b, b, b, a, a,
+    a, a, a, a, a, a, a, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect7 = [
+    b, b, b, b, b, b, b, a,
+    b, a, a, a, a, a, b, a,
+    b, a, a, a, a, a, b, a,
+    b, a, a, a, a, a, b, a,
+    b, a, a, a, a, a, b, a,
+    b, a, a, a, a, a, b, a,
+    b, b, b, b, b, b, b, a,
+    a, a, a, a, a, a, a, a
+    ]
+
+rect8 = [
+    b, b, b, b, b, b, b, b,
+    b, a, a, a, a, a, a, b,
+    b, a, a, a, a, a, a, b,
+    b, a, a, a, a, a, a, b,
+    b, a, a, a, a, a, a, b,
+    b, a, a, a, a, a, a, b,
+    b, a, a, a, a, a, a, b,
+    b, b, b, b, b, b, b, b
+    ]
+
 while(1):
-    a = 1
-    for x in range(0,8):
-        for y in range(0,a):
-            a += 2
-            sense.set_pixel(y, x, (100,100,100))
-            sense.set_pixel(x, y, (100,100,100))
-        sleep(0.2)
-    sleep(2)
+    sense.set_pixels(rect1)
+    sleep(0.1)
+    sense.set_pixels(rect2)
+    sleep(0.1)
+    sense.set_pixels(rect3)
+    sleep(0.1)
+    sense.set_pixels(rect4)
+    sleep(0.1)
+    sense.set_pixels(rect5)
+    sleep(0.1)
+    sense.set_pixels(rect6)
+    sleep(0.1)
+    sense.set_pixels(rect7)
+    sleep(0.1)
+    sense.set_pixels(rect8)
+    sleep(0.1)
+    
+    sense.set_pixels(rect7)
+    sleep(0.1)
+    sense.set_pixels(rect6)
+    sleep(0.1)
+    sense.set_pixels(rect5)
+    sleep(0.1)
+    sense.set_pixels(rect4)
+    sleep(0.1)
+    sense.set_pixels(rect3)
+    sleep(0.1)
+    sense.set_pixels(rect2)
+    sleep(0.1)
+    sense.set_pixels(rect1)
+    sleep(0.1)
