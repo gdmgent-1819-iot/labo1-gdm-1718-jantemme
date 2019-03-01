0b8afd7f64daea89b4284586ddc8fa383e2b2e5b
gdm-1718-jantemme <jantemme@student.arteveldehs.be>
2019-03-01 22:30:38 +0100

    Final

--- avatar_animated.py
+++ avatar_animated.py
@@ -10,6 +10,7 @@ def randNumb(endOfRange):
     return random.randint(0,endOfRange)
 
 while(1):
+    color = (randNumb(255),randNumb(255),randNumb(255))
     for y in range(0,8):
         for x in range(0,4):
             state = randNumb(1)
@@ -19,6 +20,6 @@ while(1):
                 sense.set_pixel((7-x), y, (0,0,0))
             
             if state == 1:
-                sense.set_pixel(x, y, (100,100,100))
-                sense.set_pixel((7-x), y, (100,100,100))
-    sleep(2)
+                sense.set_pixel(x, y, color)
+                sense.set_pixel((7-x), y, color)
+    sleep(1)
--- rect_animated.py
+++ rect_animated.py
@@ -1,136 +1,29 @@
 from sense_hat import SenseHat
 from time import sleep
-import random
 
 sense = SenseHat()
 
 sense.clear()
 
-def randNumb(endOfRange):
-    return random.randint(0,endOfRange)
-
-## algoritme niet gevonden, hardcoden dan maar
-
-a = (0,0,0)
 b = (100,100,100)
 
-rect1 = [
-    b, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect2 = [
-    b, b, a, a, a, a, a, a,
-    b, b, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect3 = [
-    b, b, b, a, a, a, a, a,
-    b, a, b, a, a, a, a, a,
-    b, b, b, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect4 = [
-    b, b, b, b, a, a, a, a,
-    b, a, a, b, a, a, a, a,
-    b, a, a, b, a, a, a, a,
-    b, b, b, b, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect5 = [
-    b, b, b, b, b, a, a, a,
-    b, a, a, a, b, a, a, a,
-    b, a, a, a, b, a, a, a,
-    b, a, a, a, b, a, a, a,
-    b, b, b, b, b, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect6 = [
-    b, b, b, b, b, b, a, a,
-    b, a, a, a, a, b, a, a,
-    b, a, a, a, a, b, a, a,
-    b, a, a, a, a, b, a, a,
-    b, a, a, a, a, b, a, a,
-    b, b, b, b, b, b, a, a,
-    a, a, a, a, a, a, a, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect7 = [
-    b, b, b, b, b, b, b, a,
-    b, a, a, a, a, a, b, a,
-    b, a, a, a, a, a, b, a,
-    b, a, a, a, a, a, b, a,
-    b, a, a, a, a, a, b, a,
-    b, a, a, a, a, a, b, a,
-    b, b, b, b, b, b, b, a,
-    a, a, a, a, a, a, a, a
-    ]
-
-rect8 = [
-    b, b, b, b, b, b, b, b,
-    b, a, a, a, a, a, a, b,
-    b, a, a, a, a, a, a, b,
-    b, a, a, a, a, a, a, b,
-    b, a, a, a, a, a, a, b,
-    b, a, a, a, a, a, a, b,
-    b, a, a, a, a, a, a, b,
-    b, b, b, b, b, b, b, b
-    ]
-
 while(1):
-    sense.set_pixels(rect1)
-    sleep(0.1)
-    sense.set_pixels(rect2)
-    sleep(0.1)
-    sense.set_pixels(rect3)
-    sleep(0.1)
-    sense.set_pixels(rect4)
-    sleep(0.1)
-    sense.set_pixels(rect5)
-    sleep(0.1)
-    sense.set_pixels(rect6)
-    sleep(0.1)
-    sense.set_pixels(rect7)
-    sleep(0.1)
-    sense.set_pixels(rect8)
-    sleep(0.1)
+    for x in range (0,8):
+        for z in range (x):
+            sense.set_pixel(z, 0, b)
+            sense.set_pixel(z, x - 1, b)
+        for y in range (x):
+            sense.set_pixel(0, y, b)
+            sense.set_pixel(x-1, y, b)
+        sleep(0.1)
+        sense.clear()
     
-    sense.set_pixels(rect7)
-    sleep(0.1)
-    sense.set_pixels(rect6)
-    sleep(0.1)
-    sense.set_pixels(rect5)
-    sleep(0.1)
-    sense.set_pixels(rect4)
-    sleep(0.1)
-    sense.set_pixels(rect3)
-    sleep(0.1)
-    sense.set_pixels(rect2)
-    sleep(0.1)
-    sense.set_pixels(rect1)
-    sleep(0.1)
+    for x in range (8, 0, -1):
+        for z in range (x):
+            sense.set_pixel(z, 0, b)
+            sense.set_pixel(z, x - 1, b)
+        for y in range (x):
+            sense.set_pixel(0, y, b)
+            sense.set_pixel(x-1, y, b)
+        sleep(0.1)
+        sense.clear()
